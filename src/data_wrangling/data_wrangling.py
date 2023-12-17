e8597d3ab8a962e1ca12b4d1951449f8593cf492
Jack Nguyen <jacknguyen0810@gmail.com>
2023-12-17 17:05:23 +0000

    gpt api file created

---
+++ src/data_wrangling/data_wrangling.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+
+class LLMDataWrangling:
+    """
+    This class class is to be used as a tool to clean raw LLM text output into machine readable vectors.
+    """
+    
+    def __init__(self, text_input: list, vector_type: str = "sentence") -> None:
+        """
+        Inputs: 
+        :param raw_text: list: Raw text input from LLM. 
+        :param vector_type: str: "sentence" (default), is to split and vectorise the text by sentence. 
+                                 "corpus", is to vectorise the whole corpus of text as one.
+                                 
+        :output dict: Dictionary containing the cleaned text output, vectors, and ID number. 
+        """
+        return
+    
+    def raw_text_2_json():
+        """
+        [content]
+        """
+        return
+    
+    
