2c86c2bf183aff153a2ad81ab6b7f1ace83e31e9
Jack Nguyen <jacknguyen0810@gmail.com>
2024-01-29 18:51:08 +0000

    tests completed + refactor

--- llm_similarity_analysis/preprocessing/text_cleaning.py
+++ llm_similarity_analysis/preprocessing/text_cleaning.py
@@ -1,10 +1,11 @@
 import string
-import contractions
 import re
 import json
+import contractions
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+import spacy
+
 
 
 class TextCleaning:
@@ -43,18 +44,15 @@ class TextCleaning:
             # Make the text lowercase
             text = text.lower()
             # Expand the contractions in the text
-            text = self.remove_symbols(text)
+            text = self.expand_contractions(text)
             # Remove symbols and ascii characters
             text = self.remove_symbols(text)
+            # Lemmatize the words in the sentence
+            text = self.lemmatize_text(text)
             # Tokenize the text data
             tokenized = word_tokenize(text)
-            # Lemmatize the words in the sentence
-            wnl = WordNetLemmatizer()
-            lemmatized = []
-            for words in tokenized:
-                lemmatized.append(wnl.lemmatize(words))
             # Remove stopwords
-            cleaned_text = self.remove_stopwords(lemmatized)
+            cleaned_text = self.remove_stopwords(tokenized)
             self.clean_text[key] = cleaned_text
         return self.clean_text
 
@@ -94,6 +92,16 @@ class TextCleaning:
                 filtered_words.append(word)
         return filtered_words
     
+    @staticmethod
+    def lemmatize_text(text: str):
+        doc_inst = spacy.load('en_core_web_sm')
+        doc = doc_inst(text)
+        tokens = []
+        for token in doc:
+            tokens.append(token)
+        lemmatized_sentence = " ".join([token.lemma_ for token in doc])
+        return lemmatized_sentence
+    
     
 if __name__ == '__main__':
     test_dict = {
@@ -108,3 +116,6 @@ if __name__ == '__main__':
     print(expanded)
     print(no_symbols)
     print(no_stop)
+    cleaner.clean()
+    for sentence in cleaner.clean_text.values():
+        print(sentence)
