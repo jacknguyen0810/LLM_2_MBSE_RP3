8182ba369fbdb3e755a170a69e99e455375ff1bb
Jack Nguyen <jacknguyen0810@gmail.com>
2024-01-29 16:17:58 +0000

    moved functions

--- src/similarity_analysis/corpus_similarity.py
+++ src/similarity_analysis/corpus_similarity.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from gensim.models.word2vec import Word2Vec
 from src.similarity_analysis.similarity_metrics import *
+from src.utilities.utility_functions import vectorise_dataset, combine_into_corpus
+
 
 
 class CorpusSimilarityAnalysis:
@@ -38,11 +40,12 @@ class CorpusSimilarityAnalysis:
 
     def run(self) -> None:
         # Combine the sentences into a text corpus
-        self.text_tokens1 = self.combine_into_corpus(self.text_tokens1)
-        self.text_tokens2 = self.combine_into_corpus(self.text_tokens2)
+        self.text_tokens1 = combine_into_corpus(self.text_tokens1)
+        self.text_tokens2 = combine_into_corpus(self.text_tokens2)
+        
         # Turn the text token datasets into vectors using Word2Vec
-        self.vectors1 = self.vectorise_dataset(self.text_tokens1)
-        self.vectors2 = self.vectorise_dataset(self.text_tokens2)
+        self.vectors1 = vectorise_dataset(self.text_tokens1)
+        self.vectors2 = vectorise_dataset(self.text_tokens2)
 
         # Extract the similarity
         comp_metric = self.similarity_metric_functions.get(
@@ -67,33 +70,7 @@ class CorpusSimilarityAnalysis:
         plt.yticks(self.text_tokens2.keys())
 
     @staticmethod
-    def combine_into_corpus(tokens: dict):
-        # Combine all values from dictionary of text tokens into one corpus
-        corpus_dict = {}  # Empty list to hold text corpus
-        corpus = []
-        for tokens in tokens.values():
-            # Combine all of the tokens to make a text corpus
-            for token in tokens:
-                corpus.append(token)
-        corpus_dict["Full Text Corpus"] = corpus
-        return corpus_dict  # Make this the text token
-
-    @staticmethod
     def metric_error(*args) -> None:
         raise ValueError("Invalid Similarity Metric")
-
-    def vectorise_dataset(self, tokens: dict) -> dict:
-        vectors = {}
-        if self.vector_type == "cbow":
-            sg = 0
-        elif self.vector_type == "skip gram":
-            sg = 1
-        else:
-            raise ValueError("Not a valid vectorization method was chosen. ")
-
-        for req_num, sentence in tokens.items():
-            vector = Word2Vec(sentence, min_count=1, vector_size=100, window=5, sg=sg)
-            vectors[req_num] = vector
-        return vectors
-
+    
     # TODO: Research appropriate vector size for full corpus
