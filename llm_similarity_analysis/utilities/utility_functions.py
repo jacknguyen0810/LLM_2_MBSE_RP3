53c8ed7c3bda2cda6a1ffb47938aaff016b8ba04
Jack Nguyen <jacknguyen0810@gmail.com>
2024-02-02 00:29:57 +0000

    vectorisation changed to sentence bert

--- llm_similarity_analysis/utilities/utility_functions.py
+++ llm_similarity_analysis/utilities/utility_functions.py
@@ -1,32 +1,39 @@
-from gensim.models.word2vec import Word2Vec
+from sentence_transformers import SentenceTransformer
 
 
-def vectorise_dataset(
-    tokens: dict,
-    vector_type: str = "cbow",
-    vector_size: int = 100,
-    min_count: int = 1,
-    window: int = 5,
-) -> dict:
-    vectors = {}
-    if vector_type == "cbow":
-        # Better for longer texts.
-        sg = 0
-    elif vector_type == "sg":
-        #  Better for shorter texts.
-        sg = 1
-    else:
-        raise ValueError("Not a valid vectorization method was chosen, please choose cbow or sg")
+def vectorise_dataset(tokens: dict, model: str = "all-mpnet-base-v2") -> dict:
+    """_summary_
 
-    for req_num, sentence in tokens.items():
-        vector = Word2Vec(
-            sentence, min_count=min_count, vector_size=vector_size, window=window, sg=sg
-        )
-        vectors[req_num] = vector
-    return vectors
+    Args:
+        tokens (dict): Dictionary containing lists of tokenized text data.
+        model (str, optional): Sentence-BERT model name. Defaults to "all-mpnet-base-v2".
+                    "all-mpnet-base-v2" is more general purpose.
+                    "allenai-specter" trained on a scientific dataset. (https://arxiv.org/abs/2004.07180)
+    Returns:
+        dict: A dictionary containing the sentence and its equivalent text embedding/vector
+    """
+    transformer = SentenceTransformer(model)
+
+    embeddings = {}
+
+    for token in tokens.values():
+        # Join the tokens together to create a string
+        sentence = " ".join(token)
+        embedding = transformer.encode(sentence)
+        embeddings[sentence] = embedding
+
+    return embeddings
 
 
-def combine_into_corpus(tokens: dict):
+def combine_into_corpus(tokens: dict) -> dict:
+    """Combine a dictionary full of tokens into a dictionary with a single value, which is all of the tokens combined.
+
+    Args:
+        tokens (dict): Tokenized text data.
+
+    Returns:
+        dict: Single list containing all of the text tokens
+    """
     # Combine all values from dictionary of text tokens into one corpus
     corpus_dict = {}  # Empty list to hold text corpus
     corpus = []
