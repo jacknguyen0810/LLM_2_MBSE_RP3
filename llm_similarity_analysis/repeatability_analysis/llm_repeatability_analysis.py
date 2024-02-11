a58a10557c85dc3a67baa802df97ba51aa324b7f
Jack Nguyen <jacknguyen0810@gmail.com>
2024-02-11 22:23:33 +0000

    refactoring changes

--- llm_similarity_analysis/repeatability_analysis/llm_repeatability_analysis.py
+++ llm_similarity_analysis/repeatability_analysis/llm_repeatability_analysis.py
@@ -8,7 +8,7 @@ class LLMRepeatabilityAnalysis:
     """A class to test the repeatability of LLM responses.
     """
     
-    def __init__(self, data_folder_fp: str, validation_case_fp: str):
+    def __init__(self, data_folder_fp: str, validation_case_fp: str = None):
         # Data is a list of .txt files that contain the raw LLM output
         self.data_folder_fp = data_folder_fp
         self.validation_case_fp = validation_case_fp
@@ -35,16 +35,19 @@ class LLMRepeatabilityAnalysis:
                 # Clean the data and add to a dictionary
                 raw_output = preprocessor.txt_to_dict(file)
                 # Filter out the terms text that aren't requirements
-                raw_output = {k: v for k, v in raw_output.items() if v[0] == '-'}
+                raw_output = {k: v for k, v in raw_output.items() if v[0:4] == '   -'}
                 cleaner = TextCleaning(text_dict=raw_output)
                 cleaned_data = cleaner.clean()
                 self.number_of_requirements[str(corpus_no)] = len(cleaned_data)
                 self.clean_data[str(corpus_no)] = cleaned_data
         
         # Get validation case:
-        raw_output = preprocessor.txt_to_dict(self.validation_case_fp)
-        cleaner = TextCleaning(raw_output)
-        validation_data = cleaner.clean()
+        if self.validation_case_fp is None:
+            validation_data = self.clean_data['0']
+        else:
+            raw_output = preprocessor.txt_to_dict(self.validation_case_fp)
+            cleaner = TextCleaning(raw_output)
+            validation_data = cleaner.clean()
               
         # Perform corpus-to-corpus comparison against the validation data
         for corpus_no, corpus in self.clean_data.values():
