f5fe5a29837db26211873e6f6ff5fb47360ccf2a
Jack Nguyen <jacknguyen0810@gmail.com>
2024-02-11 22:06:30 +0000

    re[eatability added

--- llm_similarity_analysis/repeatability_analysis/llm_repeatabilioty_analysis.py
+++ llm_similarity_analysis/repeatability_analysis/llm_repeatabilioty_analysis.py
@@ -1,7 +1,7 @@
 import os
 from llm_similarity_analysis.preprocessing.text_preprocessing import RawData2Python
 from llm_similarity_analysis.preprocessing.text_cleaning import TextCleaning
-from llm_similarity_analysis.similarity_analysis.sentence_similarity import SentenceSimilarityAnalysis
+from llm_similarity_analysis.similarity_analysis.corpus_similarity import CorpusSimilarityAnalysis
 
 
 class LLMRepeatabilityAnalysis:
@@ -11,10 +11,12 @@ class LLMRepeatabilityAnalysis:
     def __init__(self, data_folder_fp: str, validation_case_fp: str):
         # Data is a list of .txt files that contain the raw LLM output
         self.data_folder_fp = data_folder_fp
+        self.validation_case_fp = validation_case_fp
         
-        # Empty dictionary to hold the processed data
+        # Empty dictionaries to hold the processed data
         self.clean_data = {}
         self.number_of_requirements = {}
+        self.similarities = {}
         
     
     def run(self):
@@ -22,7 +24,7 @@ class LLMRepeatabilityAnalysis:
         directory = os.fsencode(self.data_folder_fp)
         # Counter variable to count the data set being used
         corpus_no = 0
-        
+        preprocessor = RawData2Python()
         # Loop through the files in the directory
         for file in os.listdir(directory):
             filename = os.fsdecode(file)
@@ -31,7 +33,6 @@ class LLMRepeatabilityAnalysis:
                 continue
             else:
                 # Clean the data and add to a dictionary
-                preprocessor = RawData2Python()
                 raw_output = preprocessor.txt_to_dict(file)
                 # Filter out the terms text that aren't requirements
                 raw_output = {k: v for k, v in raw_output.items() if v[0] == '-'}
@@ -39,10 +40,14 @@ class LLMRepeatabilityAnalysis:
                 cleaned_data = cleaner.clean()
                 self.number_of_requirements[str(corpus_no)] = len(cleaned_data)
                 self.clean_data[str(corpus_no)] = cleaned_data
-                
-        # Perform corpus-to-corpus comparison
-    
         
-                
-                
-        
+        # Get validation case:
+        raw_output = preprocessor.txt_to_dict(self.validation_case_fp)
+        cleaner = TextCleaning(raw_output)
+        validation_data = cleaner.clean()
+              
+        # Perform corpus-to-corpus comparison against the validation data
+        for corpus_no, corpus in self.clean_data.values():
+            analysis = CorpusSimilarityAnalysis(validation_data, list(corpus.values()[str(corpus_no)]))
+            analysis.run()
+            self.similarities[str(corpus_no)] = analysis.output()
