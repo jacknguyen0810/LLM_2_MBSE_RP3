5f92dc06ce412a7a6dc6de066ff5747894117b9d
Jack Nguyen <jacknguyen0810@gmail.com>
2024-03-28 18:03:17 +0000

    type hinting

--- llm_similarity_analysis/similarity/similarity_metrics.py
+++ llm_similarity_analysis/similarity/similarity_metrics.py
@@ -6,17 +6,17 @@ from sklearn.metrics.pairwise import (
 )
 
 
-def cosine_sim(data1, data2):
+def cosine_sim(data1: list, data2: list):
     return cosine_similarity(data1, data2)
 
 
-def rbf(data1, data2):
+def rbf(data1: list, data2: list):
     return rbf_kernel(data1, data2)
 
 
-def euclidean_dist(data1, data2):
+def euclidean_dist(data1: list, data2: list):
     return euclidean_distances(data1, data2)
 
 
-def manhattan_dist(data1, data2):
+def manhattan_dist(data1: list, data2: list):
     return manhattan_distances(data1, data2)
